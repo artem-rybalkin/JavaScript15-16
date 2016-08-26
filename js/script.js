e0541bf3c789f9437da7292d81e78e21706292b0
Artem Rybalkin <artem.rybalkin86@gmail.com>
2016-08-26 16:29:35 +0300

    second commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 
+var xmlhttp;
 // Кроссбраузерная функция создания XMLHttpRequest
 function getXmlHttp(){
   var xmlhttp;
@@ -18,21 +19,15 @@ function getXmlHttp(){
 }
 // Кроссбраузерная функция создания XMLHttpRequest//
 ////////////////////////////////////////////////////////
-var button = document.getElementById('button1');
 var str;
-var xmlhttp;
+var button = document.getElementById('button1');
 
-function ask() {
-  var zapros = document.getElementById('field1');
-  var zapros1 = zapros.getAttribute('value');
-  var str = 'https://pixabay.com/api/?key=3168519-28357a1b07f864f5473734c78&q='+zapros1+'&image_type=photo';
-  return(str);
- 
-}
 function ask1(){
+  var zapros = document.getElementById('field1').value;
+  var str = 'https://pixabay.com/api/?key=3168519-28357a1b07f864f5473734c78&q='+zapros+'&image_type=photo';
 
   var xmlhttp = getXmlHttp();
-  xmlhttp.open('GET', ask(str) , true);
+  xmlhttp.open('GET', str , true);
   xmlhttp.send(null);
   
   xmlhttp.onreadystatechange = function() {
@@ -41,8 +36,7 @@ function ask1(){
         var  data = JSON.parse(xmlhttp.responseText);
         var im = document.getElementById('image1'); 
         im.setAttribute('src', data.hits[0].previewURL);
-        alert(im.getAttribute('src'));
-
+        alert(data.hits[0].previewURL);
         } else { alert('bad');}
     } 
   }
